b8ed1336a10673e23378f1427c0bdb51aa535abf
Bartłomiej Dębowski <bartek.993@gmail.com>
2021-06-06 22:34:11 +0200

    zrobione na ifach i forach

---
+++ test1.py
@@ -0,0 +1,11 @@
+
+class Wychowawca:
+    def __init__(self, imie, klasy):
+        self.imie = imie
+        self.klasy = klasy
+
+
+
+wychowaca1 = Wychowawca("Krzysztof Baczynski", ["1a", "2b"])
+print(wychowaca1.imie)
+print(wychowaca1.klasy)
